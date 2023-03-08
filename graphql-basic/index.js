4724e4f3c90a5e0b275192b6853d9460250b0a21
dashfuentes <smis117112@gmail.com>
2023-03-07 21:14:19 -0600

    feat:create couple queries in the server

--- graphql-basic/index.js
+++ graphql-basic/index.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const app = express();
 const { buildSchema } = require( 'graphql' );
 const { graphqlHTTP } = require( 'express-graphql' );
+const {books} = require('./data.json')
 
 
 const schema = buildSchema( `
@@ -9,20 +10,39 @@ const schema = buildSchema( `
     type Query {
         getWelcome:String
         getAge : Int
+        getPersonalInfo(name:String, age:Int): String
+        getBooks: [Book]
     }
+
+    type Book {
+        id: Int
+        title: String
+        author: String
+        date: String
+    }
+
+
 `)
 
 function getWelcomeEmployee() {
     return 'hello world!!'
 }
 function getAge() {
-     return 1+ 3
+     return 1 + 3
+}
+function getPersonalInfo(args) {
+    return "Hello" + args.name + "your age is: " + args.age
+}
+function getBooks(){
+return books
 }
  
 const root = {
     //Metodos de schema / funciones que resolveran esos metodos
     getWelcome: getWelcomeEmployee,
-    getAge: getAge
+    getAge: getAge,
+    getPersonalInfo,
+    getBooks: getBooks
 }
 
 app.use( '/api/graphql', graphqlHTTP( {
